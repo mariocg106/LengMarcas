19873b06326b70752017b0928fa7c59f74df29d1
mario cervantes <mariocg106@gmail.com>
2021-02-03 10:19:06 +0100

    orden

---
+++ LengMarcas/HTML/jquery/Portfolio jquery/js/app.js
@@ -0,0 +1,26 @@
+let doc = $(document);
+        
+    doc.ready(iniciar);
+
+    function iniciar() {
+        console.log('documento iniciado'); 
+        $("main").load("html/home.html")
+        let opcion = $("header #header-content nav a").on('click', mostrarMenu);
+        let home = $("header #home").on('click', mostrarMenu)
+        let navicon = $("header #navicon").on('click', mostrarNavPrincipal)
+
+    }
+
+    function mostrarMenu() {
+        console.log('clickeado'); 
+    
+        let id = $(this).attr('id');
+        console.log(id);
+        let ruta = "html/"+id+".html";
+        $("header #header-content nav a").removeClass("active");
+        $(this).siblings().removeClass("active");
+        $(this).addClass('active')
+        console.log(ruta);
+    
+        $("main").load(ruta)
+    }
