0a7622092016d0ea36eb96a6b3511f874d24ee41
mario cervantes <mariocg106@gmail.com>
2021-02-24 10:04:31 +0100

    php lib

---
+++ libreria/apilibreria/src/App/app.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+
+$app = AppFactory::create();
+
+//$app->addRoutingMiddleware();
+//$errorMidleware = $app->addErrorMiddleware(true, true, true);
+
+require __DIR__ . '/../Routes/libros.php';
+
+
+$app->run();
+
+
---
+++ libreria/apilibreria/src/config/DB.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Config;
+use \PDO;
+use App\Config\Config;
+
+class DB {
+    protected $pdo;
+    //establece la conexion al hacer un new DB() desde el controlador
+    public function __construct() {
+        //OPCIONES EN COMO MYSQL LE PASA DATOS A PHP(APACHE)
+        $opciones = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //que PDO lance las excepciones si hay errores
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ //los resultados los regrese en un objeto
+        ];
+        //cadena de conexion a la base de datos
+        $dsn = "mysql:host=".config::DB_HOST.
+        ";port=".config::DB_PORT.
+        ";dbname=".config::DB_NAME.
+        ";charset=".config::DB_CHARSET;
+        //se establece la conexion a la bd
+        $this->pdo = new PDO($dsn, config::DB_USER, config::DB_PASSWD, $opciones);
+    }
+    public function run($sql, $args = []){
+        // Ejecutará las consulta $sql con parámetros que le pasemos
+        //sql --> select * from articulos where id = ? and precio > ?
+        //En args meteremos variables del where de una consulta --> $args = [10, 56.7]
+        if (!$args){
+            $data = $this->pdo->query($sql);
+            return $data;
+        }
+    
